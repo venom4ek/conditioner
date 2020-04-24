9af6eccc8697d48685fa5155a70c5018eb605e35
venomek <venom4ek@gmail.com>
2020-04-24 05:04:34 +0300

    delited ParametrizedTest

--- src/test/java/ru/netology/ConditionerTest.java
+++ src/test/java/ru/netology/ConditionerTest.java
@@ -1,12 +1,8 @@
 package ru.netology;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ConditionerTest {
 
@@ -18,26 +14,70 @@ class ConditionerTest {
         conditioner.setCurrentTemperature(20);
     }
 
-
-    @ParameterizedTest(name = "[{index}]{0}")
-    @CsvFileSource(resources = "/home/runner/work/conditioner/conditioner/src/test/resource/increase.csv")
-    void increaseCurrentTemperature1(String test, int maxTemp, int minTemp, int curTemp, int expected) {
+    @Test
+    void increaseCurrentTemperature() {
         Conditioner conditioner = new Conditioner();
-        conditioner.setMaxTemperature(maxTemp);
-        conditioner.setMinTemperature(minTemp);
-        conditioner.setCurrentTemperature(curTemp);
+        conditioner.setMaxTemperature(26);
+        conditioner.setMinTemperature(15);
+        conditioner.setCurrentTemperature(17);
         conditioner.increaseCurrentTemperature();
+        int expected = 18;
         assertEquals(expected, conditioner.getCurrentTemperature());
     }
 
-    @ParameterizedTest(name = "[{index}]{0}")
-    @CsvFileSource(resources = "../../../resource/decrease.csv")
-    void decreaseCurrentTemperature1(String test, int maxTemp, int minTemp, int curTemp, int expected) {
+    @Test
+    void increaseCurrentTemperatureMax() {
         Conditioner conditioner = new Conditioner();
-        conditioner.setMaxTemperature(maxTemp);
-        conditioner.setMinTemperature(minTemp);
-        conditioner.setCurrentTemperature(curTemp);
+        conditioner.setMaxTemperature(26);
+        conditioner.setMinTemperature(15);
+        conditioner.setCurrentTemperature(26);
+        conditioner.increaseCurrentTemperature();
+        int expected = 26;
+        assertEquals(expected, conditioner.getCurrentTemperature());
+    }
+
+    @Test
+    void increaseCurrentTemperatureOverMax() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setMaxTemperature(26);
+        conditioner.setMinTemperature(15);
+        conditioner.setCurrentTemperature(32);
+        conditioner.increaseCurrentTemperature();
+        int expected = 26;
+        assertEquals(expected, conditioner.getCurrentTemperature());
+    }
+
+
+    @Test
+    void decreaseCurrentTemperature() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setMaxTemperature(26);
+        conditioner.setMinTemperature(15);
+        conditioner.setCurrentTemperature(17);
         conditioner.decreaseCurrentTemperature();
+        int expected = 16;
+        assertEquals(expected, conditioner.getCurrentTemperature());
+    }
+
+    @Test
+    void decreaseCurrentTemperatureMin() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setMaxTemperature(26);
+        conditioner.setMinTemperature(15);
+        conditioner.setCurrentTemperature(15);
+        conditioner.decreaseCurrentTemperature();
+        int expected = 15;
+        assertEquals(expected, conditioner.getCurrentTemperature());
+    }
+
+    @Test
+    void decreaseCurrentTemperatureUnderMin() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setMaxTemperature(26);
+        conditioner.setMinTemperature(15);
+        conditioner.setCurrentTemperature(10);
+        conditioner.decreaseCurrentTemperature();
+        int expected = 15;
         assertEquals(expected, conditioner.getCurrentTemperature());
     }
 
